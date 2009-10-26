7421d9be20afc084a4ee6955dd4a5995c50b7309
elia.ignacio <elia.ignacio@a48ef308-a3ca-11de-b98f-4500b42dc123>
2009-10-26 15:57:55 +0000

    Improved test.

--- tests/test-opts.sh
+++ tests/test-opts.sh
@@ -18,26 +18,36 @@ OPTS_EXP=('Specify the build directory' 'Specify the branch name' \
 	'This help' 'Do not perform any updates')
 #------------------------------------------------------------------------------
 
+# Globals ---------------------------------------------------------------------
+let used_opts=0
+#------------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------
 function opts_fn_() {
   if [ "${*}" != "" ]; then
     echo "The rest: ${*}"
+  elif [ ${used_opts} -eq 0 ]; then
+    get_opts_print_usage 1>&2
   fi
 }
 
 function opts_fn_branch() {
+  let ++used_opts
   echo "Branch: ${1}"
 }
 
 function opts_fn_build() {
+  let ++used_opts
   echo "Build: ${1}"
 }
 
 function opts_fn_help() {
+  let ++used_opts
   get_opts_print_usage 1>&2
 }
 
 function opts_fn_noupdate() {
+  let ++used_opts
   echo "No Update"
 }
 #------------------------------------------------------------------------------
