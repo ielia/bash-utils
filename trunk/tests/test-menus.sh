9730bb0cac7baf160b10ddc2265ee7a564bc2988
elia.ignacio@gmail.com <elia.ignacio@gmail.com@a48ef308-a3ca-11de-b98f-4500b42dc123>
2011-03-21 19:09:48 +0000

    Fixed bug in lib-ascii.

--- trunk/tests/test-menus.sh
+++ trunk/tests/test-menus.sh
@@ -3,7 +3,7 @@
 . ../lib-ascii.sh
 . ../lib-menus.sh
 
-CMDS=('cmd A' 'cmd B' 'cmd C' 'cmd D' 'quit')
+CMDS=('cmd A' 'cmd B' 'cmd C' 'cmd D' '5' '6' '7' '8' '9' '10' 'quit')
 PS3="Choose: "
 PS5="Invalid option: "
 choice=0 # Set it to a valid index != quit
