756351e19223ace83a79735d87d624cc00268b49
elia.ignacio@gmail.com <elia.ignacio@gmail.com@a48ef308-a3ca-11de-b98f-4500b42dc123>
2011-03-27 14:14:30 +0000

    Added multi letter input.

--- trunk/tests/test-menus.sh
+++ trunk/tests/test-menus.sh
@@ -3,7 +3,7 @@
 . ../lib-ascii.sh
 . ../lib-menus.sh
 
-CMDS=('cmd A' 'cmd B' 'cmd C' 'cmd D' '5' '6' '7' '8' '9' '10' 'quit')
+CMDS=($(seq 0 100) 'quit')
 PS3="Choose: "
 PS5="Invalid option: "
 choice=0 # Set it to a valid index != quit
