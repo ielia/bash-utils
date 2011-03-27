b2f4fadc2022a5d8104dc2b8403290f4265b4cca
elia.ignacio@gmail.com <elia.ignacio@gmail.com@a48ef308-a3ca-11de-b98f-4500b42dc123>
2011-03-27 14:14:30 +0000

    Added multi letter input.

--- lib-menus.sh
+++ lib-menus.sh
@@ -9,28 +9,62 @@
 
 #------------------------------------------------------------------------------
 # Arguments:
+#   * Number of letters per choice
 #   * List of menu items
 function print_menu() {
-  local choice=0
+  local letters=${1}
+  shift
+  let --letters
+  local choice=()
+  local i
+  for ((i=0; $i<=${letters}; ++i)) {
+    choice[${i}]=32
+  }
+  choice[${letters}]=96
   local string
+  let i=${letters}
   for string in "${@}"; do
-    let ++choice
-    printf "$(chr $((${choice}+96)))) %s\n" "${string}"
+    let ++choice[${i}]
+    while [ ${choice[${i}]} -gt 122 ]; do
+      choice[${i}]=97
+      let --i
+      if [ ${choice[${i}]} -lt 97 ]; then
+        let choice[${i}]=97
+      else
+        let ++choice[${i}]
+      fi
+    done
+    let i=${letters}
+    for char in ${choice[*]}; do
+      echo -n "$(chr ${char})"
+    done
+    echo ") ${string}"
   done
 }
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
 # Arguments:
+#   * Number of letters per choice
 #   * Number of choices
 function get_choice_number() {
-  local choices=${1}
+  local letters_per_choice=${1}
+  local choices=${2}
   local choice
-  read -p "${PS3}" -n 1 choice
-  local choice_number=$(($(ord ${choice})-97))
-  if [ ${choice_number} -lt 0 ]; then
-    let choice_number+=32
-  fi
+  read -e -p "${PS3}" -n ${letters_per_choice} choice
+  local letters=${#choice}
+  local choice_number=-1
+  local choice_numbers=()
+  local i=0
+  for ((i=0; ${i}<${letters}; ++i)) {
+    local choice_numbers[${i}]=$(($(ord ${choice:${i}:1})-97))
+    if [ ${choice_numbers[${i}]} -lt 0 ]; then
+      let choice_numbers[${i}]+=32
+    fi
+    let ++choice_number
+    let choice_number*=26
+    let choice_number+=choice_numbers[${i}]
+  }
   echo
   if [ ${choice_number} -ge 0 -a ${choice_number} -le ${choices} ]; then
     return ${choice_number}
@@ -49,9 +83,11 @@ function select_letter() {
   local choice_strings=("${@}")
   local choices=${#choice_strings[@]}
   local choice_number=65535
+  local letters_per_choice=$(echo -e "scale=0\nl(${choices}-1)/l(26)+1" | bc -l)
+  let letters_per_choice=${letters_per_choice%%.*}
   while [ ${choice_number} -lt 0 -o ${choice_number} -ge ${choices} ]; do
-    print_menu "${choice_strings[@]}"
-    get_choice_number ${choices}
+    print_menu ${letters_per_choice} "${choice_strings[@]}"
+    get_choice_number ${letters_per_choice} ${choices}
     let choice_number=${?}
   done
   return ${choice_number}
